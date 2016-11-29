611531d47fd48fd21d0ccc91937ecfaa3346c8f5
Yomi Eluwande <iamlese@yahoo.com>
2016-11-29 22:25:26 +0100

    migration kini

---
+++ db/migrate/20161129211355_scraper_entry.rb
@@ -0,0 +1,4 @@
+class ScraperEntry < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
---
+++ db/migrate/20161129211517_create_rates.rb
@@ -0,0 +1,9 @@
+class CreateRates < ActiveRecord::Migration[5.0]
+  def change
+    create_table :rates do |t|
+      t.string "fxrate"
+
+      t.timestamps
+    end
+  end
+end
